e3207b794b31bb4e6839b01ab1c6e0009a1785e4
Nick Satinover <nsatinover.aws@gmail.com>
2020-12-31 23:16:24 -0500

    final

--- js/app.js
+++ js/app.js
@@ -1,7 +1,45 @@
 const $odd = $('a:odd');
-// odd.hide();
 const $secureLinks = $('a[href^="https://"]');
-// $secureLinks.hide();
 const $pdfs = $('a[href$="pdf"]');
-$pdfs.hide();
-$odd
+const $pdfCheckbox = $('<label><input type="checkbox">Allow PDF downloads</label>');
+
+
+
+const $liElems = $('li');
+$liElems.hide();
+
+const $liElemsHide = $('li:hidden');
+$liElemsHide.show();
+
+$secureLinks.attr('target', '_blank');
+$pdfs.attr('download', 'true');
+
+$odd.css('backgroundColor', 'lightgrey');
+
+$secureLinks.addClass('secure');
+$pdfs.addClass('pdf');
+
+const $submit = $(".submit-btn");
+$submit.attr('disabled', false);
+
+$pdfs.on('click', (event) => {
+    
+    if($(':checked').length === 0) {
+        event.preventDefault();
+        alert('Please check the box to allow PDF downloads');
+    }
+
+});
+
+$('#links').append($pdfCheckbox);
+
+// $('a').each( (i, link) => {
+//     // console.log(`Index ${i} for link: ${$(link).attr('href')}`);
+//     const url = $(link).attr('href');
+//     $(link).parent().append(`<p>${url}</p>`);
+// });
+
+$('a').each( function() {
+    const url = $(this).attr('href');
+    $(this).parent().append(`<p>(${url})</p>`);
+});
